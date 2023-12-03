434cb7e196d9447765b27560fe10efa5114b8009
soleilsolis <fukuman23@gmail.com>
2023-12-03 18:17:25 +0800

    Created Exam pages

---
+++ public/build/assets/AuthenticationCardLogo.7e97783f.js
@@ -0,0 +1,1 @@
+import{j as o,d as r}from"./app.c9439412.js";function e(){return o(r,{href:"/",children:o("img",{className:"w-16 h-16",src:"/storage/logo.png"})})}export{e as A};
---
+++ public/build/assets/useTypedPage.94cb41ed.js
@@ -0,0 +1,1 @@
+import{q as e}from"./app.c9439412.js";function o(){return e()}export{o as u};
